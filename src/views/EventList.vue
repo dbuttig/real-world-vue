35c234fbd5c2fc1939281fa0fe5afb6e65864377
Daniel Buttig <dbuttig@northpointkc.com>
2026-02-18 12:46:40 -0600

    API Calls with Axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   name: "EventList",
@@ -16,44 +17,17 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: "animal welfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friend at this event.",
-          location: "Meow Town",
-          date: "January 28, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kay Laydee",
-        },
-        {
-          id: 4582797,
-          category: "food",
-          title: "Community Gardening",
-          description:
-            "Join us for a day of community gardening and fresh produce.",
-          location: "Greenfield Park",
-          date: "February 12, 2022",
-          time: "10:00",
-          petsAllowed: true,
-          organizer: "Fern Pollin",
-        },
-        {
-          id: 8419988,
-          category: "sustainability",
-          title: "Beach Cleanup",
-          description:
-            "Join us for a community beach cleanup and help protect our environment.",
-          location: "Sunny Beach",
-          date: "March 15, 2022",
-          time: "09:00",
-          petsAllowed: false,
-          organizer: "Carey Wales",
-        },
-      ],
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
 };
 </script>
