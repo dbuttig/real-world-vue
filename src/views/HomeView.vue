62d724bd05a74d4f7bce4fc513889cb71dfcd169
Daniel Buttig <dbuttig@northpointkc.com>
2026-02-18 11:58:36 -0600

    Single File Components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kay Laydee",
+        },
+        {
+          id: 4582797,
+          category: "food",
+          title: "Community Gardening",
+          description:
+            "Join us for a day of community gardening and fresh produce.",
+          location: "Greenfield Park",
+          date: "February 12, 2022",
+          time: "10:00",
+          petsAllowed: true,
+          organizer: "Fern Pollin",
+        },
+        {
+          id: 8419988,
+          category: "sustainability",
+          title: "Beach Cleanup",
+          description:
+            "Join us for a community beach cleanup and help protect our environment.",
+          location: "Sunny Beach",
+          date: "March 15, 2022",
+          time: "09:00",
+          petsAllowed: false,
+          organizer: "Carey Wales",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
